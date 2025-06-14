4d52da571dced528269eb8dc8dedd87fc630b961
AllanAzo17 <aazofeifa60857@ufide.ac.cr>
2025-06-14 14:20:53 -0600

    Add project files.

---
+++ AvanceFinal.Models/Tarea.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvanceFinal.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public String Package { get; set; }
+        public String DateDeliver { get; set; }
+        public int Priority { get; set; }
+        public String Status { get; set; }
+    }
+}
