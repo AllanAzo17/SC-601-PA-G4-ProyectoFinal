aece4015b1e75edc25e757dfcfbef0f502121e82
Horacio <157430552+hporrasm@users.noreply.github.com>
2025-06-16 15:12:14 -0600

    Rename al proyecto, corrección del MVC y DataModel

---
+++ ProyectoFinal.Business/TaskBusiness.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.Business
+{
+    
+    public class TaskBusiness
+    {
+        private readonly List<Task> tasks;
+
+        public TaskBusiness()
+        {
+            
+        }
+        public void FillTask()
+        {
+           // tasks.AddRange.
+        }
+    }
+}
